9b546180385974ede007ed3d6b15342beea8de57
CBunting <bunt88@gmail.com>
2024-01-15 08:14:24 -0700

    Added drop down and javascript to get all breeds

--- doggos.js
+++ doggos.js
@@ -1,23 +1,48 @@
-const BREEDS_URL = 'https://dog.ceo/api/breeds/image/random';
+// const BREEDS_URL = 'https://dog.ceo/api/breeds/image/random';
 
-function addDoggo () {
+// function addDoggo () {
 
-    fetch(BREEDS_URL)  //fetch gets whats called a promise
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(data) {
-            const img = document.createElement('img');
-            img.src = data.message;
-            img.alt = 'Cute Doggo';
+//     //show loading spinner
+//     fetch(BREEDS_URL)  //fetch gets whats called a promise
+//         .then(function(response) {
+//             return response.json();
+//         })
+//         .then(function(data) {
+//             const img = document.createElement('img');
+//             img.src = data.message;
+//             img.alt = 'Cute Doggo';
     
-            document.querySelector('.doggos')
-                .appendChild(img);
-        })
+//             document.querySelector('.doggos')
+//                 .appendChild(img);
 
-}
+//         //stop showing loading spinner
+//         })
+
+// }
 
 
-document.querySelector('.add-doggo').addEventListener("click", addDoggo)
+// document.querySelector('.add-doggo').addEventListener("click", addDoggo)
+
+const BREEDS_URL = 'https://dog.ceo/api/breeds/list/all';
+const select = document.querySelector('.breeds');
+
+fetch(BREEDS_URL) 
+    .then(function (response) {
+        return response.json();
+})
+    .then(function (data) {
+        const breedsObject = data.message;
+        const breedsArray = Object.keys(breedsObject);
 
 
+        for (let i = 0; i < breedsArray.length; i++) {
+            const option = document.createElement('option');
+            option.value = breedsArray[i];
+            option.innerText = breedsArray[i];
+            select.appendChild(option);
+        }
+})
+
+select.addEventListener("change", function(event) {
+    console.log(event.target.value);
+});
